986d90ab2bd2f14ccf9cff8e1f6921c12dfbe47c
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-10-27 09:11:25 -0400

    Use vendored scripts

--- r2rml/pipeline-ice/00-setup.sh
+++ r2rml/pipeline-ice/00-setup.sh
@@ -8,10 +8,12 @@ set -eu
 CURDIR=`dirname "$0"`
 cd $CURDIR
 
+eval $( ../../vendor/biobricks-script-lib/activate.sh )
+
 BRICK_NAME="ice"
 if [ ! -r 'brick-data' ]; then
 	biobricks install "$BRICK_NAME"
-	ln -vsT "`../script/biobrick-path "$BRICK_NAME"`" brick-data;
+	ln -vsT "`biobrick-path "$BRICK_NAME"`" brick-data;
 else
 	cat <<EOF >&2
 brick-data for $BRICK_NAME already exists at `realpath -s brick-data`
