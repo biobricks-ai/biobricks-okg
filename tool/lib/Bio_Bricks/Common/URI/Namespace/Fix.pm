666de3c3f61afd323f257f65154989dbe9cb4e19
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-11-08 17:56:42 -0500

    Split tool/ to biobricks-ai/biobricks-okg-tool

--- tool/lib/Bio_Bricks/Common/URI/Namespace/Fix.pm
+++
@@ -1,6 +0,0 @@
-package Bio_Bricks::Common::URI::Namespace::Fix;
-# ABSTRACT: A fix for clean up of URI::Namespace
-
-sub URI::Namespace::DESTROY { }
-
-1;
--- tool/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PERL_STRICT=1 perl -MCarp::Always ./bin/simple-rml-dump.pl --base-dir ../../../ctdbase-kg/ctdbase-kg --output-file test.ttl ;
-clear;
-< test.ttl $JENA_HOME/bin/riot --syntax=Turtle --formatted=Turtle
