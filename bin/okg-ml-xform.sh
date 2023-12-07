19e5695618e53b7f2bab8871e28a80295a2e203f
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-12-07 12:51:43 -0500

    Mapping file to CSV
    This helps give an overview of the unmapped elements.

--- bin/okg-ml-xform.sh
+++ bin/okg-ml-xform.sh
@@ -5,6 +5,37 @@ set -eu
 CURDIR=`dirname "$0"`
 #CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # works in sourced files, only works for bash
 
-DATASET='ice';
+export DATASET='ice';
 
-json_xs -f yaml < okg-ml.yaml  | jq '.datasets.'"$DATASET"'.inputs."data-source/ice/DART_Data.parquet".elements|.[] | to_entries | .[] | { (.key) : .value.mapper } ' -c | grep -v '{}'
+json_xs -f yaml < okg-ml.yaml  | jq '[ .datasets[$ENV.DATASET].inputs | to_entries| .[] | {
+	(.key): 
+		[
+			.value.elements| .[] | to_entries | .[] |
+				{ (.key) :
+					(
+						.value.mapper | to_entries | .[0].value |
+							if has("class")
+							then .class
+							else
+								if has("value")
+								then .value
+								else null
+								end
+							end
+					)
+				}
+		]
+} ] ' \
+	| jq -r '["file", "element", "map_cv"], ( .[] | to_entries | .[]
+		| (.value | .[] | to_entries | .[]) as $element
+		|
+			{
+				file: .key,
+				element: $element.key,
+			}
+		| . + { map_cv : $element.value }
+		| to_entries | map(.value)
+		) | @csv' \
+	| vd -f csv
+
+ #| json_xs -t yaml
