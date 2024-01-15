0914a68f783fb07dd9cbd23c6f36f7dd27333cc4
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-01-15 09:48:01 -0500

    Fuseki: increase heap space to 128 GB

--- run-fuseki.sh
+++ run-fuseki.sh
@@ -5,7 +5,7 @@ export JENA_FUSEKI_PATH=$( perl -MAlien::Jena::Fuseki -e 'print Alien::Jena::Fus
 export FUSEKI_HOME=$JENA_FUSEKI_PATH;
 export FUSEKI_BASE=$PWD/run;
 
-exec /usr/bin/java -Xmx4G \
+exec /usr/bin/java -Xmx128G \
 	-Dlog4j.configurationFile=$JENA_FUSEKI_PATH/log4j2.properties \
 	-cp $JENA_FUSEKI_PATH/fuseki-server.jar:$HDT_JAVA_PATH/lib/hdt-api-3.0.10.jar:$HDT_JAVA_PATH/lib/hdt-java-core-3.0.10.jar:$HDT_JAVA_PATH/lib/hdt-jena-3.0.10.jar \
 	org.apache.jena.fuseki.cmd.FusekiCmd "$@"
