6494dbdd01408e2e02b6901c9a59ff3f9a0e7284
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-01-16 14:26:31 -0500

    Add triple count stats

--- report/2024-01-15/sum-report.sh
+++ report/2024-01-15/sum-report.sh
@@ -8,9 +8,9 @@ CURDIR=`dirname "$0"`
 BY="void#triples"
 
 echo dataset,triples
-echo hgnc,$(./bin/sum-hdtInfo-by data-source/internal-hgnc-kg/ $BY)
-echo tox21,$(./bin/sum-hdtInfo-by data-source/internal-tox21-kg/ $BY)
-echo uniprot-kg,$(./bin/sum-hdtInfo-by data-source/uniprot-kg/ $BY)
-echo mesh-kg,$(./bin/sum-hdtInfo-by data-source/mesh-kg/ $BY)
-echo ice,$(./bin/sum-hdtInfo-by brick/ice.hdt $BY)
-echo toxcast,$(./bin/sum-hdtInfo-by brick/toxcast.hdt $BY)
+echo hgnc,$(./bin/rdf-hdt-info-sum-by data-source/internal-hgnc-kg/ $BY)
+echo tox21,$(./bin/rdf-hdt-info-sum-by data-source/internal-tox21-kg/ $BY)
+echo uniprot-kg,$(./bin/rdf-hdt-info-sum-by data-source/uniprot-kg/ $BY)
+echo mesh-kg,$(./bin/rdf-hdt-info-sum-by data-source/mesh-kg/ $BY)
+echo ice,$(./bin/rdf-hdt-info-sum-by brick/ice.hdt $BY)
+echo toxcast,$(./bin/rdf-hdt-info-sum-by brick/toxcast.hdt $BY)
