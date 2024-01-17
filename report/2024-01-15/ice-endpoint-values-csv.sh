a9bcfe302dfd667ea74c92766d15d1a7fc309c0d
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-01-17 10:28:21 -0500

    Indent and use `--csv` option

--- report/2024-01-15/ice-endpoint-values-csv.sh
+++ report/2024-01-15/ice-endpoint-values-csv.sh
@@ -3,24 +3,24 @@
 set -eu
 
 duckdb -csv -c "$(cat <<'EOF'
-SELECT DISTINCT file_name, name
-FROM parquet_schema('data-source/ice/*.parquet')
-WHERE
-name LIKE '%Endpoint%'
-AND
-file_name NOT LIKE '%MOA%'
-AND
-file_name NOT LIKE '%cHTS%'
-AND
-file_name NOT LIKE '%Tox21MT%'
-AND
-file_name NOT LIKE '%Chemical_Functional_Use_Categories_Data%'
+	SELECT DISTINCT file_name, name
+	FROM parquet_schema('data-source/ice/*.parquet')
+	WHERE
+		name LIKE '%Endpoint%'
+		AND
+		file_name NOT LIKE '%MOA%'
+		AND
+		file_name NOT LIKE '%cHTS%'
+		AND
+		file_name NOT LIKE '%Tox21MT%'
+		AND
+		file_name NOT LIKE '%Chemical_Functional_Use_Categories_Data%'
 EOF
 )" \
 	| awk 'NR>1' \
-	| parallel --colsep ',' '
+	| parallel --csv '
 duckdb -csv -c "$(cat <<EOF
-SELECT DISTINCT '\''{1}'\'',  '\"'{2}'\"' FROM "{1}";
+	SELECT DISTINCT '\''{1}'\'',  '\"'{2}'\"' FROM "{1}";
 EOF
 )" | awk "NR>1"
 '  | awk 'BEGIN { print "File,Value" } { print }'
