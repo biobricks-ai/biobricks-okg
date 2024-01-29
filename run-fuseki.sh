31ad4dd3116765fd20fbd28ef2fc253926720bf0
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-01-29 10:33:02 -0500

    Check min value for `vm.max_map_count`

--- run-fuseki.sh
+++ run-fuseki.sh
@@ -9,6 +9,23 @@ export JAVA_OPTIONS=${JENA_FUSEKI_JAVA_OPTIONS:-"-Xmx128G"}
 # -Xmx192G
 # -Xss256k
 
+if uname -o | grep -qi 'linux'; then
+	KEY='vm.max_map_count'
+	MIN_VALUE="$(( 256 * 1024 ))" # 256k
+	CUR_VALUE=$(sysctl -n $KEY)
+	if [ $CUR_VALUE -lt $MIN_VALUE ]; then
+		cat <<EOF
+## Current value for $KEY=$CUR_VALUE ($(( $CUR_VALUE / 1024 ))k) is too low.
+##
+## Set $KEY to $(( $MIN_VALUE / 1024 ))k by running:
+
+    $ sudo sysctl -w $KEY=$MIN_VALUE
+
+EOF
+		exit 1
+	fi
+fi
+
 exec /usr/bin/java $JAVA_OPTIONS \
 	-Dlog4j.configurationFile=$JENA_FUSEKI_PATH/log4j2.properties \
 	-cp $JENA_FUSEKI_PATH/fuseki-server.jar:$HDT_JAVA_PATH/lib/hdt-api-3.0.10.jar:$HDT_JAVA_PATH/lib/hdt-java-core-3.0.10.jar:$HDT_JAVA_PATH/lib/hdt-jena-3.0.10.jar \
