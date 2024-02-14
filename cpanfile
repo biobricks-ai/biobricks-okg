b59cbafa7de085e733eec46c6c40bc4f7881d795
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-02-14 10:23:23 -0500

    Add cpanfile deps to flake

--- cpanfile
+++ cpanfile
@@ -1,3 +1,5 @@
+# NOTE: Also listed in flake.nix.
+
 # for cpanel_json_xs
 requires 'Cpanel::JSON::XS';
 requires 'YAML';
--- flake.nix
+++ flake.nix
@@ -1,9 +1,25 @@
 {
   description = "biobricks-okg BioBrick";
 
-  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
-
-  outputs = { self, dev-shell }: {
-    devShells = dev-shell.devShells;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    dev-shell.url = "github:biobricks-ai/dev-shell";
   };
+
+  outputs = { self, nixpkgs, flake-utils, dev-shell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
+          (oldAttrs: {
+            buildInputs = oldAttrs.buildInputs ++ [
+              perlPackages.CpanelJSONXS    # see cpanfile
+              perlPackages.YAML            # see cpanfile
+              perlPackages.TemplateToolkit # see db-fuseki/cpanfile
+              jq
+              (lib.hiPrio pkgs.parallel-full) # prefer GNU Parallel over `moreutils`
+              moreutils
+            ];
+          });
+      });
 }
